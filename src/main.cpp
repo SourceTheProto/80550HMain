09c027fccd031721ecdff2128789496332d040e6
SourceTheProto <abicknell05@proton.me>
2023-11-28 17:37:48 -0700

    Naming Changes * Motor names changed * Variable names for motor speed changed - User can no longer press B to exit (redundant feature)

--- src/main.cpp
+++ src/main.cpp
@@ -1,27 +1,27 @@
 // Macros
 #define stopDriveMotors(brakeMethod)  \
-  rightMotorA.setStopping(brakeMethod);   \
-  rightMotorB.setStopping(brakeMethod);   \
-  leftMotorA.setStopping(brakeMethod);   \
-  leftMotorB.setStopping(brakeMethod);   \
-  rightMotorA.stop();                     \
-  rightMotorB.stop();                     \
-  leftMotorA.stop();                     \
-  leftMotorB.stop()
+  MotorFR.setStopping(brakeMethod);   \
+  MotorRR.setStopping(brakeMethod);   \
+  MotorFL.setStopping(brakeMethod);   \
+  MotorRL.setStopping(brakeMethod);   \
+  MotorFR.stop();                     \
+  MotorRR.stop();                     \
+  MotorFL.stop();                     \
+  MotorRL.stop()
 
 #define spinDriveMotors(Direction)  \
-  rightMotorA.spin(Direction);        \
-  rightMotorB.spin(Direction);        \
-  leftMotorA.spin(Direction);        \
-  leftMotorB.spin(Direction)
+  MotorFR.spin(Direction);        \
+  MotorRR.spin(Direction);        \
+  MotorFL.spin(Direction);        \
+  MotorRL.spin(Direction)
 
 #define rightSide(speed, unit)      \
-  rightMotorA.setVelocity(speed, unit); \
-  rightMotorB.setVelocity(speed, unit)
+  MotorFR.setVelocity(speed, unit); \
+  MotorRR.setVelocity(speed, unit)
 
 #define leftSide(speed, unit)       \
-  leftMotorA.setVelocity(speed, unit); \
-  leftMotorB.setVelocity(speed, unit)
+  MotorFL.setVelocity(speed, unit); \
+  MotorRL.setVelocity(speed, unit)
 
 #include "vex.h"
 using namespace vex;
@@ -31,14 +31,15 @@ brain Brain;
 // Hardware
 controller Controller1 = controller(primary);
 
-motor leftMotorA = motor(PORT20, ratio18_1, false);
-motor leftMotorB = motor(PORT14, ratio18_1, false);
-motor_group LeftDriveSmart = motor_group(leftMotorA, leftMotorB);
-motor rightMotorA = motor(PORT1, ratio18_1, true);
-motor rightMotorB = motor(PORT16, ratio18_1, true);
-motor_group RightDriveSmart = motor_group(rightMotorA, rightMotorB);
-inertial DrivetrainInertial = inertial(PORT3);
-smartdrive Drivetrain = smartdrive(LeftDriveSmart, RightDriveSmart, DrivetrainInertial, 319.19, 320, 40, mm, 1);
+motor MotorFL = motor(PORT20, ratio18_1, false);
+motor MotorRL = motor(PORT14, ratio18_1, false);
+motor MotorFR = motor(PORT1, ratio18_1, true);
+motor MotorRR = motor(PORT16, ratio18_1, true);
+
+inertial Motion = inertial(PORT3);
+motor_group LeftDriveSmart = motor_group(MotorFL, MotorRL);
+motor_group RightDriveSmart = motor_group(MotorFR, MotorRR);
+smartdrive Drivetrain = smartdrive(LeftDriveSmart, RightDriveSmart, Motion, 319.19, 320, 40, mm, 1);
 
 digital_out Wings = digital_out(Brain.ThreeWirePort.A);
 
@@ -93,16 +94,16 @@ void autonomous(void) {
 }
 
 void driverControl(void) {
-  rightMotorA.setMaxTorque(MOTOR_TORQUE, percent);
-  rightMotorB.setMaxTorque(MOTOR_TORQUE, percent);
-  leftMotorA.setMaxTorque(MOTOR_TORQUE-(MOTOR_TORQUE/5.5), percent);
-  leftMotorB.setMaxTorque(MOTOR_TORQUE-(MOTOR_TORQUE/5.5), percent);
+  MotorFR.setMaxTorque(MOTOR_TORQUE, percent);
+  MotorRR.setMaxTorque(MOTOR_TORQUE, percent);
+  MotorFL.setMaxTorque(MOTOR_TORQUE-(MOTOR_TORQUE/5.5), percent);
+  MotorRL.setMaxTorque(MOTOR_TORQUE-(MOTOR_TORQUE/5.5), percent);
 
   while (true) {
     // Drive Control
     // Setting variables
-    leftSpeed = (Controller1.Axis3.position()*speedMult) + (Controller1.Axis1.position()*turnMult);
-    rightSpeed = (Controller1.Axis3.position()*speedMult) - (Controller1.Axis1.position()*turnMult);
+    leftSpeed = (Controller1.Axis3.position()*speedMultiplier) + (Controller1.Axis1.position()*spinMultiplier);
+    rightSpeed = (Controller1.Axis3.position()*speedMultiplier) - (Controller1.Axis1.position()*spinMultiplier);
     // Applying Movement
     if (fabs(leftSpeed)+fabs(rightSpeed) > 5) {
       leftSide(leftSpeed, percent);
@@ -116,8 +117,8 @@ void driverControl(void) {
 
 // Settings
 const int MOTOR_TORQUE = 80;
-const double speedMult = .3;
-const double turnMult = .3;
+const double speedMultiplier = .3;
+const double spinMultiplier = .3;
 
 // Global Variables
 double leftSpeed = 0;
@@ -129,7 +130,7 @@ const int TURN_ERROR = -6;
 const int SCREENX = 480;
 const int SCREENY = 272;
 
-// Functions
+// thread callbacks
 void temperatureMonitor();
 
 int main() {
@@ -138,6 +139,7 @@ int main() {
   Competition.drivercontrol(driverControl);
   
   pre_auton();
+  driverControl();
 
   while (true) {
     wait(100, msec);
@@ -150,24 +152,22 @@ void temperatureMonitor() {
   Brain.Screen.setFillColor(black);
   Brain.Screen.drawRectangle(0, 0, SCREENX, SCREENY);
   while (true) {
-    motorTempAvg = (rightMotorA.temperature(percent) + leftMotorA.temperature(percent)
-                    + rightMotorB.temperature(percent) + leftMotorB.temperature(percent)) / 4; // Adds up & divides all temperatures for average
+    motorTempAvg = (MotorFR.temperature(percent) + MotorFL.temperature(percent)
+                    + MotorRR.temperature(percent) + MotorRL.temperature(percent)) / 4; // Adds up & divides all temperatures for average
     
     Brain.Screen.setFont(mono40);
     Brain.Screen.setPenColor(white);
 
     // Printing all motor temperatures & average
     Brain.Screen.setCursor(1, 1);
-    Brain.Screen.print("%d", static_cast<int>(rightMotorA.temperature(percent)));
+    Brain.Screen.print("%d", static_cast<int>(MotorFR.temperature(percent)));
     Brain.Screen.setCursor(7, 1);
-    Brain.Screen.print("%d", static_cast<int>(leftMotorA.temperature(percent)));
+    Brain.Screen.print("%d", static_cast<int>(MotorFL.temperature(percent)));
     Brain.Screen.setCursor(1, 23);
-    Brain.Screen.print("%d", static_cast<int>(rightMotorB.temperature(percent)));
-    Brain.Screen.setCursor(9, 23);
-    Brain.Screen.print("%d", static_cast<int>(leftMotorB.temperature(percent)));
+    Brain.Screen.print("%d", static_cast<int>(MotorRR.temperature(percent)));
+    Brain.Screen.setCursor(7, 23);
+    Brain.Screen.print("%d", static_cast<int>(MotorRL.temperature(percent)));
     Brain.Screen.setCursor(3, 7);
     Brain.Screen.print("MotorAVG: %d", static_cast<int>(motorTempAvg));
-
-    if (Controller1.ButtonB.pressing()) {vexSystemExitRequest();}
   }
 }
